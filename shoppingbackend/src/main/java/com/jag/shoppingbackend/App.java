44e03910bee9bd053617754157479f626b6e08e9
Jagendra Khadka <jagendra.khadka@gmail.com>
2018-07-18 16:10:59 -0400

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/jag/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.jag.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
